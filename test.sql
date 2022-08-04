2f4c6b7bf1e6675f0eb6bea40ab532f62f6224ae
futromain <101249833+futromain@users.noreply.github.com>
2022-08-04 17:28:18 +0200

    sql

---
+++ test.sql
@@ -0,0 +1,9 @@
+-- Databricks notebook source
+-- MAGIC %sql
+-- MAGIC select date, value, userType, kpiName
+-- MAGIC from prod_ds.user_kpis_per_customer
+-- MAGIC where customerName like 'H&M%'
+-- MAGIC     and kpiName = "MAU"
+-- MAGIC order by date, userType
+
+-- COMMAND ----------
