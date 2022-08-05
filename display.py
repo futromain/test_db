52a868bf3d29561d16cc81c7bea159aafc0c37fa
futromain <101249833+futromain@users.noreply.github.com>
2022-08-05 10:11:54 +0200

    comment 2

--- display.py
+++ display.py
@@ -1,7 +1,7 @@
 # Databricks notebook source
 # MAGIC %sql
 # MAGIC select date, value, userType, kpiName
-# MAGIC from prod_ds.user_kpis_per_customer
+# MAGIC from prod_ds.user_kpis_per_customer -- another comment
 # MAGIC where customerName like 'H&M%'
 # MAGIC     and kpiName = "MAU" -- comment
 # MAGIC order by date, userType
