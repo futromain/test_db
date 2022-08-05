4d1ebf230c6b53f357df36863d5c10681fa1debd
futromain <101249833+futromain@users.noreply.github.com>
2022-08-05 10:09:29 +0200

    Update display.py

--- display.py
+++ display.py
@@ -3,7 +3,7 @@
 # MAGIC select date, value, userType, kpiName
 # MAGIC from prod_ds.user_kpis_per_customer
 # MAGIC where customerName like 'H&M%'
-# MAGIC     and kpiName = "MAU"
+# MAGIC     and kpiName = "MAU" -- comment
 # MAGIC order by date, userType
 
 # COMMAND ----------
